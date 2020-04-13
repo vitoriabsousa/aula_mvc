7650a61cade7482c2e7883484fc0746b0eef97f1
Vitória Sousa <62552717+vitoriabsousa@users.noreply.github.com>
2020-04-13 19:03:04 -0300

    Add files via upload

--- ProjetoMVC/public/index.php
+++ ProjetoMVC/public/index.php
@@ -1,15 +1,18 @@
 <?php
 
+header ("Context-Type: text/html; charset=utf-8");
 require_once("../src/vendor/autoload.php");
 require_once("../config/config.php");
 
-use src\classes\ClassRoutes;
+$Dispatch = new app\Dispatch();
 
-class Teste extends ClassRoutes{
-        public function __construct(){
-            $v=$this->getRota();
-            var_dump($v);
-        }
-}
-$new = new Teste();
-?>
+// use src\classes\ClassRoutes;
+
+// class Teste extends ClassRoutes{
+//         public function __construct(){
+//             $v=$this->getRota();
+//             var_dump($v);
+//         }
+// }
+// $new = new Teste();
+// ?>
