32fea81b3373a785788e1d796a094de3db6d175c
Vitória Sousa <62552717+vitoriabsousa@users.noreply.github.com>
2020-04-18 12:10:18 -0300

    Add files via upload

--- ProjetoMVC/app/Controller/ControllerSitemap.php
+++ ProjetoMVC/app/Controller/ControllerSitemap.php
@@ -3,8 +3,8 @@
 namespace app\Controller;
 
 class ControllerSitemap {
-    public function __construct () {
-        echo "teste!";
+    public function testeMetodo ($Par1, $Par2, $Par3) {
+        echo "O parâmetro 1 é <strong> {$Par1} </strong>, o parâmetro 2 é <strong> {$Par2} </strong>, O parâmetro 3 é <strong> {$Par3} </strong>";
     }
 }
 
