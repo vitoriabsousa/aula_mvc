35811bff5095f30fb8489d6dd749336e388307a4
Vitória Sousa <62552717+vitoriabsousa@users.noreply.github.com>
2020-05-03 23:07:19 -0300

    Add files via upload

--- ProjetoMVC/public/index.php
+++ ProjetoMVC/public/index.php
@@ -6,13 +6,4 @@ require_once("../config/config.php");
 
 $Dispatch = new app\Dispatch();
 
-use src\classes\ClassRoutes;
-
-class Teste extends ClassRoutes{
-        public function __construct(){
-            $v=$this->getRota();
-            var_dump($v);
-        }
-}
-$new = new Teste();
 ?>
